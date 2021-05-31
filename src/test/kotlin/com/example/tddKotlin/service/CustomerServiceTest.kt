5363d0146a07964463c2cfbaa2948116e56ac03f
Palma <JPALMA7@ford.com>
2021-05-31 18:38:55 -0400

    mockk

--- src/test/kotlin/com/example/tddKotlin/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/example/tddKotlin/service/CustomerServiceTest.kt
@@ -2,16 +2,15 @@ package com.example.tddKotlin.service
 
 import com.example.tddKotlin.model.Customer
 import com.example.tddKotlin.repository.CustomerRepository
+import io.mockk.every
+import io.mockk.mockk
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-//import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.util.ReflectionTestUtils
 
 @RunWith(MockitoJUnitRunner::class)
 internal class CustomerServiceTest {
@@ -19,13 +18,14 @@ internal class CustomerServiceTest {
     @Mock
     lateinit var customerService: CustomerService
 
-    @Mock
-    lateinit var customerRepository : CustomerRepository
+    val customerRepository = mockk<CustomerRepository>()
 
     @Test
     fun `getAllCustomers should return the list of customers`() {
         val expectedCustomers = listOf(Customer("Jimmy"), Customer("Javier"))
-        `when`(customerRepository.findAll()).thenReturn(expectedCustomers)
+//        ReflectionTestUtils.setField(customerService, "customerRepository", customerRepository)
+//        `when`(customerRepository.findAll()).thenReturn(expectedCustomers)
+        every { customerRepository.findAll() } returns expectedCustomers
         assertEquals(expectedCustomers, customerService.getAllCustomers())
     }
 }
